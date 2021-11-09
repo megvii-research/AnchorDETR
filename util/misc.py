fc3d45441241cd689b28878d3aa4b0bffb33a8b8
Honghe <leopardsaga@gmail.com>
2021-11-09 11:55:54 +0800

    Fix semantic version checking.

--- util/misc.py
+++ util/misc.py
@@ -19,6 +19,7 @@ from collections import defaultdict, deque
 import datetime
 import pickle
 from typing import Optional, List
+from packaging import version
 
 import torch
 import torch.nn as nn
@@ -27,7 +28,7 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.5:
+if version.parse(torchvision.__version__) < version.parse('0.5'):
     import math
     from torchvision.ops.misc import _NewEmptyTensorOp
     def _check_size_scale_factor(dim, size, scale_factor):
@@ -54,7 +55,7 @@ if float(torchvision.__version__[:3]) < 0.5:
         return [
             int(math.floor(input.size(i + 2) * scale_factors[i])) for i in range(dim)
         ]
-elif float(torchvision.__version__[:3]) < 0.7:
+elif version.parse(torchvision.__version__) < version.parse('0.7'):
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
